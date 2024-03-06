73d9b9b83a003b4cc7428f66072ea6db3b06b0c6
Okashe <okashabdi88@gmail.com>
2024-03-06 11:13:39 -0600

    initial commit

---
+++ src/main/java/okash/example/movies/MoviesApplication.java
@@ -0,0 +1,17 @@
+package okash.example.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
