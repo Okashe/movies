0b4e7dc0cf756c43437755bc29d09b378afa774f
Okashe <okashabdi88@gmail.com>
2024-03-06 12:52:50 -0600

    initial commit

---
+++ src/main/java/okash/example/movies/MovieRepository.java
@@ -0,0 +1,8 @@
+package okash.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
